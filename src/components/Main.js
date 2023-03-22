101f2574d4e6594621b059819207f0ae1c40c2ac
Victor Ramos <ramos_victor@outlook.com>
2023-03-22 13:08:26 -0300

    Lógica de geração de números

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,27 @@
 export default function Main(){
+    
+    let numbers = []
+
+    let steps = () =>{
+
+        let newNumber = Math.floor((Math.random())*61)
+    
+        if(numbers.includes(newNumber) === false && numbers.length < 6 ){
+            numbers.push(newNumber)
+            steps()
+        } else if(numbers.includes(newNumber) === true && numbers.length < 6){
+            steps()
+        } else{
+            console.log('Fim da criação de números') 
+            console.log(numbers.join(', '))
+        } 
+    }
+
+    steps()
+    
     return(
         <>
+        {numbers.join(', ')}
         </>
     )
 }
