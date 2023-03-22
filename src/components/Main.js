ecb73768197576b29c6bdab24bd6a020b737ddac
Victor Ramos <ramos_victor@outlook.com>
2023-03-22 18:47:23 -0300

    Aplicação de design

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,14 @@
 import { useState } from 'react'
+import styles from './Main.module.css'
+import logo01 from '../img/logo01.png'
 
 export default function Main(){
 
         let armazenarNumeros = []
+        let date = new Date()
 
-        const [mostrarResultado, setMostrarResultado] = useState()
+        const [mostrarResultado, setMostrarResultado] = useState('Nenhuma sequência gerada')
+        const [mostrarHorario, setMostrarHorario] = useState('Nenhum horário para mostrar')
 
         function gerarNumeros(){
 
@@ -18,15 +22,20 @@ export default function Main(){
                 gerarNumeros()
 
             } else{
-                setMostrarResultado(armazenarNumeros.sort((a, b) => a - b).join(', '))
+                setMostrarResultado(armazenarNumeros.sort((a, b) => a - b).join(' - '))
+                setMostrarHorario(`${date.getHours()}:${date.getMinutes()}`)
             }       
         } 
 
     return(
-        <>
-          <h1>Números gerados: </h1>
-          <h3>{mostrarResultado}</h3>
-          <button className="btn" onClick={gerarNumeros}>Gerar números</button>
-        </>
+        <div className={styles.containerNativo}>
+            <div className={styles.containerDosElementos}>
+            <img src={logo01} alt="Logo da Megasena" className={styles.logo}></img>
+            <h1>Números gerados: </h1>
+            <h3 className={styles.resultado}>{mostrarResultado}</h3>
+            <button className={styles.btn} onClick={gerarNumeros}>Gerar números</button>
+            <h5 className={styles.horario}>Números gerados às: {mostrarHorario}</h5>
+            </div>
+        </div>
     )
 }    
