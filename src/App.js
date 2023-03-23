bef25c08b954f16b2c5e2dee942fca91fdb640a8
Victor Ramos <ramos_victor@outlook.com>
2023-03-23 13:06:10 -0300

    Criação de responsividade

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Main from './components/Main'
 
 function App() {
   return (
-    <div className="App">
-    <Main/>  
+    <div className="App">  
+    <Main/> 
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,12 +5,16 @@ import logo01 from '../img/logo01.png'
 export default function Main(){
 
         let armazenarNumeros = []
-        let date = new Date()
+        let data = new Date()
 
-        const [mostrarResultado, setMostrarResultado] = useState('Nenhuma sequência gerada')
-        const [mostrarHorario, setMostrarHorario] = useState('Nenhum horário para mostrar')
+        const [mostrarResultado, setMostrarResultado] = useState('Gere uma sequência')
+        const [mostrarHorario, setMostrarHorario] = useState('Sem horário')
 
         function gerarNumeros(){
+
+            function ajusteMinutos(){
+                return (data.getMinutes() < 10) ? `0${data.getMinutes()}` : data.getMinutes()
+            }
 
             let numeroGerado = Math.ceil((Math.random()) * 60)
     
@@ -23,18 +27,18 @@ export default function Main(){
 
             } else{
                 setMostrarResultado(armazenarNumeros.sort((a, b) => a - b).join(' - '))
-                setMostrarHorario(`${date.getHours()}:${date.getMinutes()}`)
-            }       
+                setMostrarHorario(`${data.getHours()}:${ajusteMinutos()}`)
+            }   
         } 
 
     return(
         <div className={styles.containerNativo}>
-            <div className={styles.containerDosElementos}>
-            <img src={logo01} alt="Logo da Megasena" className={styles.logo}></img>
-            <h1>Números gerados: </h1>
-            <h3 className={styles.resultado}>{mostrarResultado}</h3>
-            <button className={styles.btn} onClick={gerarNumeros}>Gerar números</button>
-            <h5 className={styles.horario}>Números gerados às: {mostrarHorario}</h5>
+            <div className={styles.containerElementos}>
+                <img src={logo01} alt="Logo da Megasena" className={styles.logo}></img>
+                <h1 className={styles.frase}>Números gerados: </h1>
+                <h3 className={styles.resultado}>{mostrarResultado}</h3>
+                <button className={styles.btn} onClick={gerarNumeros}>Gerar números</button>
+                <h5 className={styles.horario}>Números gerados às: {mostrarHorario}</h5>
             </div>
         </div>
     )
